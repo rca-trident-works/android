cc7460ca7cc77fb4de9bcd32f9faa204b65e1bb2
ろむねこ <nem@nem.x0.to>
2023-11-02 14:00:04 +0900

    いろいろ

---
+++ LectureButton1/app/src/main/java/com/example/lecturebutton1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lecturebutton1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            Toast.makeText(this, "リスナー: ボタン1が押されました", Toast.LENGTH_SHORT).show();
+        }
+    }
+}

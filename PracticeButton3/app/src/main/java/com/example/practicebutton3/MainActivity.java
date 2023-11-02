1caf441424d99b7a5a0e915a2271157eab9c8f98
ろむねこ <nem@nem.x0.to>
2023-11-02 15:07:09 +0900

    PracticeButton3

--- PracticeButton3/app/src/main/java/com/example/practicebutton3/MainActivity.java
+++ PracticeButton3/app/src/main/java/com/example/practicebutton3/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.practicebutton3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SeekBar seekBar = findViewById(R.id.seekBar1);
+                    Toast.makeText(MainActivity.this, "体重: " + seekBar.getProgress() + "kg", Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+
+        findViewById(R.id.button2).setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SeekBar seekBar = findViewById(R.id.seekBar1);
+                    seekBar.setProgress(0);
+                    Toast.makeText(MainActivity.this, "リセットしました。", Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
     }
 }

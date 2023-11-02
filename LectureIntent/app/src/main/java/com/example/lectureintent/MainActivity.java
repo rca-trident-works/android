6713048f9a4ae1d3a100e7aad343240acb34c349
ろむねこ <nem@nem.x0.to>
2023-11-02 16:04:09 +0900

    いろいろ

---
+++ LectureIntent/app/src/main/java/com/example/lectureintent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lectureintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonSecond).setOnClickListener(
+            (View view) -> {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        );
+    }
+}

0718dd6b7f7e6b34e5fc8275823d5a33e21056a8
ろむねこ <nem@nem.x0.to>
2023-11-02 14:30:22 +0900

    PracticeButton2

--- PracticeButton2/app/src/main/java/com/example/practicebutton2/MainActivity.java
+++ PracticeButton2/app/src/main/java/com/example/practicebutton2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.practicebutton2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText editText1 = (EditText)findViewById(R.id.editText1);
+                        TextView textView1 = (TextView)findViewById(R.id.textView1);
+
+                        textView1.setText(editText1.getText());
+                    }
+                }
+        );
     }
 }

75c0ed30f9a315d8e5f6f0bc01283c0f0eb9bfd5
ろむねこ <nem@nem.x0.to>
2023-11-02 14:07:05 +0900

    LectureButton1

---
+++ LectureButton2/app/src/main/java/com/example/lecturebutton2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lecturebutton2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "無名クラス: ボタン1が押されました", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+}

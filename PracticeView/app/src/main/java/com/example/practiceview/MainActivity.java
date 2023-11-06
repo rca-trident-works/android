ba8b0d3693146706a0b8b450f827d15543f84537
ろむねこ <nem@nem.x0.to>
2023-11-06 15:59:26 +0900

    とちゅう

--- PracticeView/app/src/main/java/com/example/practiceview/MainActivity.java
+++ PracticeView/app/src/main/java/com/example/practiceview/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.practiceview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public WebView getPrimaryWebView() {
+        return findViewById(R.id.primaryWebView);
+    }
+
+    public void updateUrlTextEdit() {
+        EditText urlTextEdit = findViewById(R.id.editTextUrl);
+        urlTextEdit.setText(getPrimaryWebView().getUrl());
+    }
+
+    public void init() {
+        getPrimaryWebView().loadUrl("https://www.google.com");
+        updateUrlTextEdit();
+
+        // URLの自動変更
+        getPrimaryWebView().setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                updateUrlTextEdit();
+            }
+        });
+
+        // Go Button
+        findViewById(R.id.buttonAccessUrl).setOnClickListener(v -> {
+            EditText urlTextEdit = findViewById(R.id.editTextUrl);
+            getPrimaryWebView().loadUrl(urlTextEdit.getText().toString());
+        });
     }
 }

753314d8dafc8599e2849e0ffe62daef49de60f8
ろむねこ <nem@nem.x0.to>
2023-11-07 10:00:28 +0900

    いろいろ

--- PracticeView/app/src/main/java/com/example/practiceview/MainActivity.java
+++ PracticeView/app/src/main/java/com/example/practiceview/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,27 +22,60 @@ public class MainActivity extends AppCompatActivity {
         return findViewById(R.id.primaryWebView);
     }
 
-    public void updateUrlTextEdit() {
+    public void updateUrlAndTitle() {
+        WebView primaryWebView = getPrimaryWebView();
         EditText urlTextEdit = findViewById(R.id.editTextUrl);
-        urlTextEdit.setText(getPrimaryWebView().getUrl());
+        urlTextEdit.setText(primaryWebView.getUrl());
+//        TextView titleTextView = findViewById(R.id.textViewTitle);
+//        titleTextView.setText(primaryWebView.getTitle());
     }
 
     public void init() {
-        getPrimaryWebView().loadUrl("https://www.google.com");
-        updateUrlTextEdit();
+        WebView primaryWebView = getPrimaryWebView();
 
-        // URLの自動変更
-        getPrimaryWebView().setWebViewClient(new WebViewClient() {
+        primaryWebView.loadUrl("https://www.google.com/");
+        primaryWebView.getSettings().setJavaScriptEnabled(true);
+        updateUrlAndTitle();
+
+
+        // == Listener ==
+
+        // WebViewの内容が切り替わったときにURLを更新する
+        primaryWebView.setWebViewClient(new WebViewClient() {
+
+            // onPageFinishedだと
             @Override
-            public void onPageFinished(WebView view, String url) {
-                updateUrlTextEdit();
+            public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
+                super.doUpdateVisitedHistory(view, url, isReload);
+                // Workaround: 500ms待機してから更新する
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                updateUrlAndTitle();
             }
         });
+
+
 
         // Go Button
         findViewById(R.id.buttonAccessUrl).setOnClickListener(v -> {
             EditText urlTextEdit = findViewById(R.id.editTextUrl);
             getPrimaryWebView().loadUrl(urlTextEdit.getText().toString());
+        });
+
+        // toggleJS
+        findViewById(R.id.switchJavaScript).setOnClickListener(v -> {
+            getPrimaryWebView().getSettings().setJavaScriptEnabled(((android.widget.Switch) v).isChecked());
+        });
+
+        findViewById(R.id.buttonBack).setOnClickListener(v -> {
+            getPrimaryWebView().goBack();
+        });
+
+        findViewById(R.id.buttonForward).setOnClickListener(v -> {
+            getPrimaryWebView().goForward();
         });
     }
 }

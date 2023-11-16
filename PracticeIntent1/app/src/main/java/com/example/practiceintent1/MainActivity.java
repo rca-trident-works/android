da0289539d6951346ca8c154626492139a6cc081
ろむねこ <nem@nem.x0.to>
2023-11-16 14:03:53 +0900

    Global変数

--- PracticeIntent1/app/src/main/java/com/example/practiceintent1/MainActivity.java
+++ PracticeIntent1/app/src/main/java/com/example/practiceintent1/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.practiceintent1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyGlobals myGlobals;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myGlobals = (MyGlobals) getApplication();
+
+        // Button1
+        findViewById(R.id.button1).setOnClickListener(v -> {
+            myGlobals.editText1Str = ((EditText) findViewById(R.id.editText1)).getText().toString();
+            Intent intent = new Intent(MainActivity.this, NextActivity.class);
+            startActivity(intent);
+        });
+
+        // ButtonFinish1
+        findViewById(R.id.buttonFinish1).setOnClickListener(v -> {
+            finish();
+        });
     }
 }
---
+++ PracticeIntent1/app/src/main/java/com/example/practiceintent1/MyGlobals.java
@@ -0,0 +1,8 @@
+package com.example.practiceintent1;
+
+import android.app.Application;
+
+public class MyGlobals extends Application {
+
+    String editText1Str = "";
+}

e76ac0c5956394191fd8d3686e276072c0eb9553
ろむねこ <nem@nem.x0.to>
2023-11-16 13:53:32 +0900

    NextNextActivity

---
+++ PracticeIntent1/app/src/main/java/com/example/practiceintent1/NextNextActivity.java
@@ -0,0 +1,14 @@
+package com.example.practiceintent1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextNextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_next);
+    }
+}

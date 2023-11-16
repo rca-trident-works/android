8316af8f0577330a7eace2e3e87a1e7764d5575b
ろむねこ <nem@nem.x0.to>
2023-11-16 14:07:38 +0900

    ロジック

--- PracticeIntent1/app/src/main/java/com/example/practiceintent1/NextActivity.java
+++ PracticeIntent1/app/src/main/java/com/example/practiceintent1/NextActivity.java
@@ -2,13 +2,36 @@ package com.example.practiceintent1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NextActivity extends AppCompatActivity {
+
+    MyGlobals myGlobals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        myGlobals = (MyGlobals) getApplication();
+
+        // ButtonShow2
+        findViewById(R.id.buttonShow2).setOnClickListener(v -> {
+            ((EditText) findViewById(R.id.editText2)).setText(myGlobals.editText1Str + "ですね");
+        });
+
+        // Button2
+        findViewById(R.id.button2).setOnClickListener(v -> {
+            myGlobals.editText1Str = ((EditText) findViewById(R.id.editText2)).getText().toString();
+            Intent intent = new Intent(NextActivity.this, NextNextActivity.class);
+            startActivity(intent);
+        });
+
+        // ButtonFinish2
+        findViewById(R.id.buttonFinish2).setOnClickListener(v -> {
+            finish();
+        });
     }
 }
--- PracticeIntent1/app/src/main/java/com/example/practiceintent1/NextNextActivity.java
+++ PracticeIntent1/app/src/main/java/com/example/practiceintent1/NextNextActivity.java
@@ -3,12 +3,28 @@ package com.example.practiceintent1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NextNextActivity extends AppCompatActivity {
+
+    MyGlobals myGlobals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_next);
+
+        myGlobals = (MyGlobals) getApplication();
+
+        // ButtonShow3
+        findViewById(R.id.buttonShow3).setOnClickListener(v -> {
+            ((TextView) findViewById(R.id.textView3)).setText(myGlobals.editText1Str + "ですね");
+        });
+
+        // ButtonFinish3
+        findViewById(R.id.buttonFinish3).setOnClickListener(v -> {
+            finish();
+        });
     }
 }

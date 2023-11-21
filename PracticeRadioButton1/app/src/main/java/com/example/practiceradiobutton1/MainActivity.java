149f8737cd7636313ceea2b02a02801bf77dbeb1
ろむねこ <nem@nem.x0.to>
2023-11-21 10:39:14 +0900

    ロジック実装した

--- PracticeRadioButton1/app/src/main/java/com/example/practiceradiobutton1/MainActivity.java
+++ PracticeRadioButton1/app/src/main/java/com/example/practiceradiobutton1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.practiceradiobutton1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((RadioGroup) findViewById(R.id.radioGroup1)).setOnCheckedChangeListener(
+                (RadioGroup radioGroup, int i) -> {
+                    if (i == R.id.radioButtonBlue) {
+                        radioGroup.setBackgroundColor(Color.BLUE);
+                    } else if (i == R.id.radioButtonRed) {
+                        radioGroup.setBackgroundColor(Color.RED);
+                    } else if (i == R.id.radioButtonYellow) {
+                        radioGroup.setBackgroundColor(Color.YELLOW);
+                    }
+                }
+        );
+
+        findViewById(R.id.buttonNext).setOnClickListener((View view) -> {
+            int i = ((RadioGroup) findViewById(R.id.radioGroup1)).getCheckedRadioButtonId();
+
+            if (i == R.id.radioButtonBlue) {
+                ((RadioButton) findViewById(R.id.radioButtonYellow)).setChecked(true);
+            } else if (i == R.id.radioButtonYellow) {
+                ((RadioButton) findViewById(R.id.radioButtonRed)).setChecked(true);
+            } else if (i == R.id.radioButtonRed) {
+                ((RadioButton) findViewById(R.id.radioButtonBlue)).setChecked(true);
+            }
+        });
     }
 }

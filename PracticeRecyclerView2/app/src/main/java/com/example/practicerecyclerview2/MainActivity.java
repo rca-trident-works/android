69264e1783a71a9ea7b965c6beee805d3abf8fec
ろむねこ <nem@nem.x0.to>
2023-11-28 10:59:12 +0900

    貼り付けた

---
+++ PracticeRecyclerView2/app/src/main/java/com/example/practicerecyclerview2/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.practicerecyclerview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView1);
+        //RecyclerViewのサイズを固定
+        recyclerView.setHasFixedSize(true);
+
+        //RecyclerViewに区切り線を入れる
+        RecyclerView.ItemDecoration itemDecoration =
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        //レイアウトマネージャを設定
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //①リスト構造(String型の可変長の配列)を宣言
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        //②リスト構造に値を追加
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+
+        //③Adapterとリスト構造を結び付け
+        //RecyclerAdapterクラスを呼び出す
+        RecyclerAdapter adapter = new RecyclerAdapter(arrayList);
+
+        //④RecyclerViewとAdapterの結び付け
+        recyclerView.setAdapter(adapter);
+
+        //追加ボタンのクリックイベントを登録
+        findViewById(R.id.btnAdd).setOnClickListener(
+                v -> {
+                    //editAddを取り出してeditに代入
+                    EditText edit = findViewById(R.id.editAdd);
+
+                    //editの入力文字をgetTextメソッドで取り出して
+                    //String型に変換してstrに代入
+                    String str = edit.getText().toString();
+
+                    //ArrayListの一番下に値を追加
+                    arrayList.add(str);
+
+                    //一番下に値が追加されたことをAdapterが画面に通知
+                    adapter.notifyItemInserted(arrayList.size());
+                }
+        );
+    }
+}
---
+++ PracticeRecyclerView2/app/src/main/java/com/example/practicerecyclerview2/RecyclerAdapter.java
@@ -0,0 +1,85 @@
+package com.example.practicerecyclerview2;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+//RecyclerView.Adapterクラスを継承
+public class RecyclerAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+
+    ArrayList<String> arrayList;
+
+    //RecyclerAdapterのコンストラクタ
+    public RecyclerAdapter(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    //新しいViewHolderを生成すると呼び出される
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //recycler_row.xmlをactivity_main.xmlの部品にする
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_row, parent, false);
+
+        //新しいViewHolderを作成
+        //ItemViewHolderクラスを呼び出す
+        ItemViewHolder holder = new ItemViewHolder(view);
+
+        //クリックイベントを登録
+        holder.itemView.setOnClickListener(v -> {
+            //クリックされた行を取得
+            int position = holder.getAdapterPosition();
+
+            //クリックされた文字をトースト表示
+            Toast.makeText(v.getContext(), arrayList.get(position), Toast.LENGTH_SHORT).show();
+
+            //クリックされた行を削除
+            arrayList.remove(position);
+
+            //行が削除されたことを画面に通知
+            notifyItemRemoved(position);
+        });
+
+        //生成したViewHolderを戻す
+        return holder;
+    }
+
+    //1行分のレイアウトの詳細設定
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        //指定された位置の値を取得
+        holder.getTextView().setText(arrayList.get(position));
+    }
+
+    //ArrayListのデータ件数を取得
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+}
+
+//RecyclerView.ViewHolderクラスを継承
+class ItemViewHolder extends RecyclerView.ViewHolder {
+    private final TextView textView;
+
+    //ItemViewHolderのコンストラクタ
+    public ItemViewHolder(View view) {
+        super(view);
+        //ViewHolderのビューにテキストを定義する
+        textView = view.findViewById(R.id.textView1);
+    }
+
+    //テキストの値を取得
+    public TextView getTextView() {
+        return textView;
+    }
+}

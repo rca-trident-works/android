186c84cc0d265ad6483dc857b4b51e5e8d4cc068
ろむねこ <nem@nem.x0.to>
2023-11-28 10:18:55 +0900

    Init

--- PracticeCheckBox1/app/src/main/java/com/example/practicecheckbox1/MainActivity.java
+++ PracticeCheckBox1/app/src/main/java/com/example/practicecheckbox1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.practicecheckbox1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //checkBoxA～checkBoxCを取り出し，cbA～cbCへ代入。
+        final CheckBox cbA = findViewById(R.id.checkBoxA);
+        final CheckBox cbB = findViewById(R.id.checkBoxB);
+        final CheckBox cbC = findViewById(R.id.checkBoxC);
+
+        //textView1を取り出し，tv1へ代入。
+        final TextView tv1 = findViewById(R.id.textView1);
+
+        cbA.setOnCheckedChangeListener(                            //【結び付け】
+                new CompoundButton.OnCheckedChangeListener() {  //【準備】
+                    @Override                                          //【処理】
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                        //重み　：  4   2   1
+                        //         □   □   □
+                        //ビュー：cbA cbB cbC
+
+                        int value = 0;  //初期値は0
+
+                        //CheckBoxCがCheckされていたら  isChecked()はtrueならcheckされている
+                        if (cbC.isChecked() == true) { //	      falseならcheckされていない
+                            value += 1; //2の0乗である1を加算する
+                        }
+
+                        //CheckBoxBがCheckされていたら　※「== true」は省略されることが多い
+                        if (cbB.isChecked()) {
+                            value += 2; //2の1乗である2を加算する
+                        }
+
+                        if (cbA.isChecked()) {
+                            value += 4; //2の2乗である4を加算する
+                        }
+
+                        //valueの値をString型に型変換し，textView1の表示文字にする。
+                        tv1.setText(String.valueOf(value));
+                    }
+                }
+        );
+
+//        cbB.setOnCheckedChangeListener(                         //【結び付け】
+//                【cbAと同じであるため，省略】
+//        );
+
+//        cbC.setOnCheckedChangeListener(                         //【結び付け】
+//                【cbAと同じであるため，省略】
+//        );
+
+        findViewById(R.id.buttonAll0).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        cbA.setChecked(false); //checkBoxAのチェック状態をfalse(OFF)にする。
+                        cbB.setChecked(false);
+                        cbC.setChecked(false);
+                    }
+                }
+        );
+
     }
+
 }

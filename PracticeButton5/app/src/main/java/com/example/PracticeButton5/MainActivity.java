41832c4a963ed7b0a9d754343efb3d9c809e9125
ろむねこ <nem@nem.x0.to>
2024-04-10 14:47:58 +0900

    PracticeButton5実装

--- PracticeButton5/app/src/main/java/com/example/PracticeButton5/MainActivity.java
+++ PracticeButton5/app/src/main/java/com/example/PracticeButton5/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.PracticeButton5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 太陽
+        Button button1 = findViewById(R.id.button1);
+        // 水星
+        Button button2 = findViewById(R.id.button2);
+        // 金星
+        Button button3 = findViewById(R.id.button3);
+        // 地球
+        Button button4 = findViewById(R.id.button4);
+        // 火星
+        Button button5 = findViewById(R.id.button5);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sun", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Mercury", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Venus", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Earth", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Mars", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

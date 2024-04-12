837c5b882993ab65e3e451a7fbda9fac40737506
ろむねこ <nem@nem.x0.to>
2024-04-12 11:01:23 +0900

    キーを定数に切り出し

---
+++ PracticeJankenApp/app/src/main/java/com/example/practicejankenapp/SyoritsuIntentConstants.java
@@ -0,0 +1,16 @@
+package com.example.practicejankenapp;
+
+public class SyoritsuIntentConstants {
+
+    // 総対戦数
+    public static final String TOTAL_BATTLE_COUNT = "totalBattleCount";
+
+    // 勝利数
+    public static final String WIN_COUNT = "winCount";
+
+    // 引き分け数
+    public static final String DRAW_COUNT = "drawCount";
+
+    // 敗北数
+    public static final String LOSE_COUNT = "loseCount";
+}

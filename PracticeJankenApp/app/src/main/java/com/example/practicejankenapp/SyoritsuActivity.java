f65777bee08c45193f7b61ee7b0e2eb9d0a054e0
ろむねこ <nem@nem.x0.to>
2024-04-12 11:05:18 +0900

    勝率アクティビティロジック実装

--- PracticeJankenApp/app/src/main/java/com/example/practicejankenapp/SyoritsuActivity.java
+++ PracticeJankenApp/app/src/main/java/com/example/practicejankenapp/SyoritsuActivity.java
@@ -2,7 +2,10 @@ package com.example.practicejankenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SyoritsuActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class SyoritsuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syoritsu);
+
+        Intent intent = getIntent();
+        int totalBattleCount = intent.getIntExtra(SyoritsuIntentConstants.TOTAL_BATTLE_COUNT, 0);
+        int winCount = intent.getIntExtra(SyoritsuIntentConstants.WIN_COUNT, 0);
+        int drawCount = intent.getIntExtra(SyoritsuIntentConstants.DRAW_COUNT, 0);
+        int loseCount = intent.getIntExtra(SyoritsuIntentConstants.LOSE_COUNT, 0);
+
+        String message = getMessage(totalBattleCount, winCount, drawCount, loseCount);
+
+        TextView textView = findViewById(R.id.resultTextView);
+        textView.setText(message);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> finish());
+    }
+
+    private String getMessage(int totalBattleCount, int winCount, int drawCount, int loseCount) {
+        return "全" + totalBattleCount + "回戦" +
+                winCount + "勝" +
+                drawCount + "分" +
+                loseCount + "敗" +
+                "勝率" + (double) winCount / totalBattleCount * 100 + "%";
     }
 }

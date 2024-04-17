e9949c807c208d38368c6e224b832fa6800ea73b
ろむねこ <nem@nem.x0.to>
2024-04-17 15:40:57 +0900

    WIP

--- PracticeFirestore1/app/src/main/java/com/example/practicefirestore1/MainActivity.java
+++ PracticeFirestore1/app/src/main/java/com/example/practicefirestore1/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
@@ -35,10 +36,15 @@ public class MainActivity extends AppCompatActivity {
         // 「保存」ボタンの処理。Firestoreにデータを保存する
         findViewById(R.id.btnSave).setOnClickListener(v -> {
             // ④Add Data - Create a new user with a first and last name
-            Map<String, Object> user = new HashMap<>();
-            user.put("first", fast.getText().toString());
-            user.put("last", last.getText().toString());
-            user.put("born", born.getText().toString());
+//            Map<String, Object> user = new HashMap<>();
+//            user.put("first", fast.getText().toString());
+//            user.put("last", last.getText().toString());
+//            user.put("born", born.getText().toString());
+
+            User user = new User();
+            user.first = fast.getText().toString();
+            user.last = last.getText().toString();
+            user.born = Integer.parseInt(born.getText().toString());
 
             // Add a new document with a generated ID
             // Firestoreのコレクション「users」に新しいドキュメントを追加する
@@ -46,12 +52,12 @@ public class MainActivity extends AppCompatActivity {
             db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                 @Override
                 public void onSuccess(DocumentReference documentReference) {
-                    Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    Log.d("@FB1", "DocumentSnapshot added with ID: " + documentReference.getId());
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Log.w("TAG", "Error adding document", e);
+                    Log.w("@FB1", "Error adding document", e);
                 }
             });
         });
@@ -62,24 +68,29 @@ public class MainActivity extends AppCompatActivity {
             // Firestoreのコレクション「users」のドキュメント一覧を取得する
             // 非同期で取得処理が動作する。結果を受け取るために処理完了時のリスナーをセットする
 
-            db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull com.google.android.gms.tasks.Task<QuerySnapshot> task) {
-                    String data = "";
-                    if (task.isSuccessful()) {
-                        for (com.google.firebase.firestore.QueryDocumentSnapshot document : task.getResult()) {
-                            Log.d("TAG", document.getId() + " => " + document.getData());
-                            data += document.getData().get("first") + " "
-                                    + document.getData().get("last") + " "
-                                    + document.getData().get("born") + "\n";
-                        }
-                        show.setText(data);
-                    } else {
-                        data = "Error getting documents: " + task.getException();
+            db.collection("users").get().addOnCompleteListener(task -> {
+                String data = "";
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        User result = document.toObject(User.class);
+                        Log.d("@FB1", document.getId() + " => " + document.getData());
+//                        data += document.getData().get("first") + " "
+//                                + document.getData().get("last") + " "
+//                                + document.getData().get("born") + "\n";
+                        data += result.first + " " + result.last + " " + result.born + "\n";
                     }
                     show.setText(data);
+                } else {
+                    data = "Error getting documents: " + task.getException();
                 }
+                show.setText(data);
             });
         });
+    }
+
+    class User {
+        public String first;
+        public String last;
+        public int born;
     }
 }

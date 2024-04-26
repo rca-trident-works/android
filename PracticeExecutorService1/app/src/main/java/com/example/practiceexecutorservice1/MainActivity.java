5780b89b5fc8355abdc35240c124f233dd041325
ろむねこ <nem@nem.x0.to>
2024-04-26 10:24:47 +0900

    貼り付け

--- PracticeExecutorService1/app/src/main/java/com/example/practiceexecutorservice1/MainActivity.java
+++ PracticeExecutorService1/app/src/main/java/com/example/practiceexecutorservice1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practiceexecutorservice1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // showButton：押されると天気情報を表示する
+        findViewById(R.id.showButton).setOnClickListener(v -> {
+
+            /// Weatherをインスタンス化
+            Weather receiver = new Weather(MainActivity.this);
+
+            String cityID = ((EditText)findViewById(R.id.inputMapCode)).getText().toString();
+
+            /// Weatherを実行する
+            receiver.execute(cityID);   /// □①実行へ
+        });
     }
 }
